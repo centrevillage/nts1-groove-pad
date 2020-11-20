ebf4216ddff01eed05a5540f45a1bcc20f8f5e2d
centrevillage <centrevillage@gmail.com>
2020-11-20 22:17:27 +0900

    add oled_ssd1306 to sdk

---
+++ firmware/lib/igb_sdk/igb_sdk/device/oled_ssd1306.hpp
@@ -0,0 +1,146 @@
+#ifndef IGB_SDK_DEVICE_OLED_SSD1306_H
+#define IGB_SDK_DEVICE_OLED_SSD1306_H
+
+namespace igb {
+namespace sdk {
+
+template<typename GPIO_PIN_TYPE, typename SPI_TYPE, size_t screen_width = 128, size_t screen_height = 64>
+struct OledSsd1306 {
+  GPIO_PIN_TYPE cs_pin;
+  GPIO_PIN_TYPE dc_pin;
+  GPIO_PIN_TYPE reset_pin;
+  SPI_TYPE spi;
+
+  uint8_t screen_buffer[screen_width * screen_height / 8];
+
+  void init() {
+    prepareGpio();
+    sendInitCommands();
+  }
+
+  void process() {
+    updateScreen();
+  }
+
+  void reset() {
+    cs_pin.high();
+    reset_pin.high();
+    delay_msec(1);
+    reset_pin.low();
+    delay_msec(10);
+    reset_pin.high();
+    delay_msec(10);
+  }
+
+  void prepareGpio() {
+    cs_pin.enable();
+    dc_pin.enable();
+    reset_pin.enable();
+
+    cs_pin.initOutput(GpioOutputMode::PUSHPULL, GpioSpeedMode::HIGH);
+    dc_pin.initOutput(GpioOutputMode::PUSHPULL, GpioSpeedMode::HIGH);
+    reset_pin.initOutput(GpioOutputMode::PUSHPULL, GpioSpeedMode::HIGH);
+  }
+
+  void sendInitCommands() {
+    reset();
+    delay_msec(100);
+
+    sendCommand(0xAE); // display off
+    sendCommand(0x20); // Set Memory Addressing Mode
+    sendCommand(0x00); // 00b,Horizontal Addressing Mode; 01b,Vertical Addressing Mode;
+                             // 10b,Page Addressing Mode (RESET); 11b,Invalid
+    sendCommand(0xB0); // Set Page Start Address for Page Addressing Mode,0-7
+    sendCommand(0xC8); // Set COM Output Scan Direction
+    sendCommand(0x00); // Set low column address
+    sendCommand(0x10); // Set high column address
+    sendCommand(0x40); // Set start line address - CHECK
+    sendCommand(0x81); // Set contrast control register - CHECK
+    sendCommand(0xFF);
+    sendCommand(0xA1); // Set segment re-map 0 to 127 - CHECK
+    sendCommand(0xA6); // Set normal color
+    //sendCommand(0xA7); // Set invert color
+    sendCommand(0xA8); // Set multiplex ratio(1 to 64) - CHECK
+    sendCommand(0x3F); //
+    sendCommand(0xA4); // 0xa4,Output follows RAM content;0xa5,Output ignores RAM content
+    sendCommand(0xD3); // Set display offset - CHECK
+    sendCommand(0x00); // Not offset
+    sendCommand(0xD5); // Set display clock divide ratio/oscillator frequency
+    //sendCommand(0xF0); // Set divide ratio
+    sendCommand(0x80); // Set divide ratio
+    sendCommand(0xD9); // Set pre-charge period
+    //sendCommand(0x22); //
+    sendCommand(0xF1); //
+    sendCommand(0xDA); // Set com pins hardware configuration - CHECK
+    sendCommand(0x12);
+    sendCommand(0xDB); // Set vcomh
+    sendCommand(0x20); // 0x20,0.77xVcc
+    sendCommand(0x8D); // Set DC-DC enable
+    sendCommand(0x14); //
+    sendCommand(0x2E); // stop scroll
+    sendCommand(0xAF); // turn on SSD1306 panel
+  }
+
+  void sendCommand(uint8_t byte) {
+    cs_pin.low(); // select OLED
+    dc_pin.low(); // command
+    spi.transferU8sync(byte);
+    cs_pin.high(); // un-select OLED
+  }
+
+  void sendCommand(uint8_t* buf, size_t buff_size) {
+    cs_pin.low(); // select OLED
+    dc_pin.high(); // data
+    spi.transferU8sync(0);
+    spi.transferU8sync(0);
+    for (size_t i = 0; i < buff_size; ++i) {
+      spi.transferU8sync(buf[i]);
+    }
+    cs_pin.high(); // un-select OLED
+  }
+
+  void sendData(uint8_t* buf, size_t buff_size) {
+    cs_pin.low(); // select OLED
+    dc_pin.high(); // data
+    spi.transferU8sync(0);
+    spi.transferU8sync(0);
+    for (size_t i = 0; i < buff_size; ++i) {
+      spi.transferU8sync(buf[i]);
+    }
+    cs_pin.high(); // un-select OLED
+  }
+
+  void updateScreen() {
+    for (uint8_t i = 0; i < screen_height/8; ++i) {
+      sendCommand(0xB0 + i); // Set the current RAM page address.
+      sendCommand(0x00);
+      sendCommand(0x10);
+      sendData(&screen_buffer[SCREEN_WIDTH*i], SCREEN_WIDTH);
+    }
+  }
+
+  void drawFillBG() {
+    for (size_t i = 0; i < sizeof(screen_buffer); ++i) {
+      screen_buffer[i] = 0;
+    }
+  }
+
+  void drawFillFG() {
+    for (size_t i = 0; i < sizeof(screen_buffer); ++i) {
+      screen_buffer[i] = 0xFF;
+    }
+  }
+
+  void drawPixel(uint8_t x, uint8_t y, uint8_t fg) {
+    if (fg) {
+      screen_buffer[x + (y / 8) * screen_width] |= 1 << (y % 8);
+    } else {
+      screen_buffer[x + (y / 8) * screen_width] &= ~(1 << (y % 8));
+    }
+  }
+};
+
+}
+}
+
+#endif /* IGB_SDK_DEVICE_OLED_SSD1306_H */
--- firmware/src/oled.cpp
+++ firmware/src/oled.cpp
@@ -8,152 +8,31 @@
 
 #include <igb_stm32/periph/systick.hpp>
 #include <igb_stm32/periph/rcc.hpp>
+#include <igb_stm32/periph/gpio.hpp>
 #include <igb_stm32/periph/spi.hpp>
+#include <igb_sdk/device/oled_ssd1306.hpp>
 
 using namespace igb::stm32;
+using namespace igb::sdk;
 
-static uint8_t oled_buffer[SCREEN_WIDTH * SCREEN_HEIGHT / 8];
-static uint8_t oled_cursor_x = 0;
-static uint8_t oled_cursor_y = 0;
-
-void oled_init();
-void oled_reset();
-void oled_send_command(uint8_t byte);
-void oled_send_data(uint8_t* buffer, size_t buff_size);
-void oled_draw_fill_bg();
-void oled_draw_fill_fg();
-void oled_set_cursor(uint8_t x, uint8_t y);
-void oled_draw_pixel(uint8_t x, uint8_t y, uint8_t fg);
-void oled_update_screen();
-
-auto oled_cs_pin = GpioPin::newPin(GpioPinType::pb9);
-auto oled_dc_pin = GpioPin::newPin(GpioPinType::pb5);
-auto oled_reset_pin = GpioPin::newPin(GpioPinType::pb8);
-auto oled_spi = Spi::newSpi(SpiType::spi1);
-
-void oled_gpio_setup() {
-  GpioPort port_b { GpioType::gpiob };
-  port_b.enable();
-
-  oled_cs_pin.initOutput(GpioOutputMode::PUSHPULL, GpioSpeedMode::HIGH);
-  oled_dc_pin.initOutput(GpioOutputMode::PUSHPULL, GpioSpeedMode::HIGH);
-  oled_reset_pin.initOutput(GpioOutputMode::PUSHPULL, GpioSpeedMode::HIGH);
-}
-
-void oled_init() {
-  oled_reset();
-  delay_msec(100);
-
-  oled_send_command(0xAE); // display off
-  oled_send_command(0x20); // Set Memory Addressing Mode
-  oled_send_command(0x00); // 00b,Horizontal Addressing Mode; 01b,Vertical Addressing Mode;
-                           // 10b,Page Addressing Mode (RESET); 11b,Invalid
-  oled_send_command(0xB0); // Set Page Start Address for Page Addressing Mode,0-7
-  oled_send_command(0xC8); // Set COM Output Scan Direction
-  oled_send_command(0x00); // Set low column address
-  oled_send_command(0x10); // Set high column address
-  oled_send_command(0x40); // Set start line address - CHECK
-  oled_send_command(0x81); // Set contrast control register - CHECK
-  oled_send_command(0xFF);
-  oled_send_command(0xA1); // Set segment re-map 0 to 127 - CHECK
-  oled_send_command(0xA6); // Set normal color
-  //oled_send_command(0xA7); // Set invert color
-  oled_send_command(0xA8); // Set multiplex ratio(1 to 64) - CHECK
-  oled_send_command(0x3F); //
-  oled_send_command(0xA4); // 0xa4,Output follows RAM content;0xa5,Output ignores RAM content
-  oled_send_command(0xD3); // Set display offset - CHECK
-  oled_send_command(0x00); // Not offset
-  oled_send_command(0xD5); // Set display clock divide ratio/oscillator frequency
-  //oled_send_command(0xF0); // Set divide ratio
-  oled_send_command(0x80); // Set divide ratio
-  oled_send_command(0xD9); // Set pre-charge period
-  //oled_send_command(0x22); //
-  oled_send_command(0xF1); //
-  oled_send_command(0xDA); // Set com pins hardware configuration - CHECK
-  oled_send_command(0x12);
-  oled_send_command(0xDB); // Set vcomh
-  oled_send_command(0x20); // 0x20,0.77xVcc
-  oled_send_command(0x8D); // Set DC-DC enable
-  oled_send_command(0x14); //
-  oled_send_command(0x2E); // stop scroll
-  oled_send_command(0xAF); // turn on SSD1306 panel
-}
-
-void oled_reset() {
-  oled_cs_pin.high();
-  oled_reset_pin.high();
-  delay_msec(1);
-  oled_reset_pin.low();
-  delay_msec(10);
-  oled_reset_pin.high();
-  delay_msec(10);
-}
-
-void oled_draw_fill_bg() {
-  for (size_t i = 0; i < sizeof(oled_buffer); ++i) {
-    oled_buffer[i] = 0;
-  }
-}
-void oled_draw_fill_fg() {
-  for (size_t i = 0; i < sizeof(oled_buffer); ++i) {
-    oled_buffer[i] = 0xFF;
-  }
-}
-void oled_set_cursor(uint8_t x, uint8_t y) {
-  oled_cursor_x = x;
-  oled_cursor_y = y;
-}
-void oled_draw_pixel(uint8_t x, uint8_t y, uint8_t fg) {
-  if (fg) {
-    oled_buffer[x + (y / 8) * SCREEN_WIDTH] |= 1 << (y % 8);
-  } else {
-    oled_buffer[x + (y / 8) * SCREEN_WIDTH] &= ~(1 << (y % 8));
-  }
-}
-void oled_update_screen() {
-  for (uint8_t i = 0; i < SCREEN_HEIGHT/8; ++i) {
-    oled_send_command(0xB0 + i); // Set the current RAM page address.
-    oled_send_command(0x00);
-    oled_send_command(0x10);
-    oled_send_data(&oled_buffer[SCREEN_WIDTH*i], SCREEN_WIDTH);
-  }
-}
-
-void oled_send_command(uint8_t byte) {
-  oled_cs_pin.low(); // select OLED
-  oled_dc_pin.low(); // command
-  oled_spi.transferU8sync(byte);
-  oled_cs_pin.high(); // un-select OLED
-}
-
-void oled_send_data(uint8_t* buffer, size_t buff_size) {
-  oled_cs_pin.low(); // select OLED
-  oled_dc_pin.high(); // data
-  oled_spi.transferU8sync(0);
-  oled_spi.transferU8sync(0);
-  for (size_t i = 0; i < buff_size; ++i) {
-    oled_spi.transferU8sync(buffer[i]);
-  }
-  oled_cs_pin.high(); // un-select OLED
-}
+OledSsd1306<GpioPin, Spi, 128, 64> ssd1306 = {
+  .cs_pin = GpioPin::newPin(GpioPinType::pb9),
+  .dc_pin = GpioPin::newPin(GpioPinType::pb5),
+  .reset_pin = GpioPin::newPin(GpioPinType::pb8),
+  .spi = Spi::newSpi(SpiType::spi1)
+};
 
 void oled_setup() {
-  oled_spi.prepareSpiMaster(GpioPinType::pa7, GpioPinType::pa6, GpioPinType::pa5, SpiBaudratePrescaler::DIV2);
-
-  oled_gpio_setup();
-  oled_init();
+  ssd1306.spi.prepareSpiMaster(GpioPinType::pa7, GpioPinType::pa6, GpioPinType::pa5, SpiBaudratePrescaler::DIV2);
+  ssd1306.init();
 }
 
-static char oled_text_buffer[32] = {0};
-static uint8_t oled_text_buffer_len = 0;
-
 void oled_process() {
-  app_screen.draw(oled_buffer);
+  app_screen.draw(ssd1306.screen_buffer);
   for (uint8_t i = 0; i < 8; ++i) {
     if (app_input.touch_bits & (1 << i)) {
-      draw_touch_pad_small(oled_buffer, (DrawPadType)i);
+      draw_touch_pad_small(ssd1306.screen_buffer, (DrawPadType)i);
     }
   }
-  //screen_draw(oled_buffer);
-  oled_update_screen();
+  ssd1306.process();
 }
